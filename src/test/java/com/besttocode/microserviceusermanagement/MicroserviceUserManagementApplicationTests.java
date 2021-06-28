061fc19db6534af70187f1887d5bd6494b734876
yacinebest <yacine.besta@gmail.com>
2021-06-28 20:36:13 +0100

    Init Project

---
+++ src/test/java/com/besttocode/microserviceusermanagement/MicroserviceUserManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.besttocode.microserviceusermanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceUserManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
